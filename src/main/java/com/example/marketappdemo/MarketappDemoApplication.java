0e01756641ce5e9f06443969777e8994a6e67a2c
zhambylbakdaulet <jambulbakdaulet@gmail.com>
2025-09-03 16:58:23 +0500

    first commit

---
+++ src/main/java/com/example/marketappdemo/MarketappDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketappDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketappDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marketappdemo/MarketappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketappDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
